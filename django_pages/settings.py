e995590d26540f281d3e9374cecef2e5186b9680
Dennis Miller <dmill98@lsu.edu>
2020-11-03 21:42:03 -0600

    configuring for heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n0)-yc*y1(h^uklaxu)^3)tv6(&73!y##$l+_q#b-$+pm-rkwd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
